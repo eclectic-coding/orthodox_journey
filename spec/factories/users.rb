45dcbb1bfb6e61eef9f6c72ab686e51302d220c7
Chuck Smith <csmit223@nd.edu>
2024-09-04 08:34:09 -0400

    Add devise trackable

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,19 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  first_name             :string
 #  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
 #  role                   :string           default("subscriber")
+#  sign_in_count          :integer          default(0), not null
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #

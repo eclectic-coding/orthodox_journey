fcc6b5851e7e09a668ce0f0df4082c51fb7564f9
Chuck <chuck@eclecticsaddlebag.com>
2024-09-09 07:19:52 -0400

    Install and setup cuprite for system tests (#20)
    Co-authored-by: Chuck Smith <csmit223@nd.edu>

---
+++ spec/system/support/capybara_setup.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Capybara settings (not covered by Rails system tests)
+
+# Make server listening on all hosts
+Capybara.server_host = "0.0.0.0"
+Capybara.server_port = 3001
+
+# Don't wait too long in `have_xyz` matchers
+Capybara.default_max_wait_time = 2
+
+# Normalizes whitespaces when using `has_text?` and similar matchers
+Capybara.default_normalize_ws = true
+
+# Where to store artifacts (e.g. screenshots, downloaded files, etc.)
+Capybara.save_path = ENV.fetch("CAPYBARA_ARTIFACTS", "./tmp/capybara")
+
+Capybara.singleton_class.prepend(Module.new do
+  attr_accessor :last_used_session
+
+  def using_session(name, &block)
+    self.last_used_session = name
+    super
+  ensure
+    self.last_used_session = nil
+  end
+end)

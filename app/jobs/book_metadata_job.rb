06fe2e9f6a1ef420f1b056dd8c0a2e9e70b1ba51
Chuck Smith <csmit223@nd.edu>
2024-09-10 15:31:44 -0400

    Add download image to active storage

--- app/jobs/book_metadata_job.rb
+++ app/jobs/book_metadata_job.rb
@@ -1,8 +1,15 @@
 # app/jobs/book_metadata_job.rb
 class BookMetadataJob < ApplicationJob
+  require 'open-uri'
+
   def perform(book_id)
     book = Book.find(book_id)
     metadata = BookMetadata.retrieve_from(book.wiki_url).attributes
-    book.update!(metadata)
+    book.update!(description: metadata[:description])
+
+    if metadata[:image]
+      downloaded_image = URI.open(metadata[:image])
+      book.image.attach(io: downloaded_image, filename: "book_#{book.id}.jpg")
+    end
   end
 end

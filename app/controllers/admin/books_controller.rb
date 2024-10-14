8765a495ce75e1351d3f40856ac7e1f85db04e1f
Chuck Smith <csmit223@nd.edu>
2024-10-14 12:55:07 -0400

    Work on admin books new form

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -14,12 +14,35 @@ class Admin::BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    p "params: #{params}"
+    @book = Book.create!(book_params)
+
+    if @book.save
+      redirect_to admin_books_path
+    else
+      render :new
+    end
+
   end
 
   def edit
   end
 
   private
+
+  def book_params
+    params.require(:book)
+          .permit(:title, :author, :authored_at, :description, :reading, :completed, :wiki_url, :reading_url)
+          .tap do |book_params|
+      if book_params[:authored_at].present?
+        book_params[:authored_at] = DateTime.new(book_params[:authored_at].to_i)
+      end
+    end
+  end
 
   def set_book
     @book = Book.find(params[:id])
---
+++ app/views/components/form_card/component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class FormCard::Component < ApplicationViewComponent
+  option :title, required: true
+end
